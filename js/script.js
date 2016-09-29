414e96d844e3421c3342e2e28407c9a2d1b7441c
Hayden Elza <hayden.elza@gmail.com>
2016-09-29 00:20:35 -0500

    Remove Upload Button on Load
    Once a dataset is selected the button is removed to avoid uploading a
    second.

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ require(['catiline'], function(cw) {
         upButton.type = "file";
         upButton.id = "input";
         upButton.onchange = function() {
-            document.getElementById("dropzone").getElementsByClassName("btn")[0].innerHTML = "+";
+            document.getElementById("dropzone").style.display = "none";
             var file = document.getElementById("input").files[0];
 
             handleFile(file);
