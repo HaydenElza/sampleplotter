9ba1563cbab6a217458413d0cebc4bcccc3394cd
Hayden Elza <hayden.elza@gmail.com>
2016-09-29 00:14:37 -0500

    Added More Tile Layers
    Added more tile layers (Thunderforest Landscape, OSM Black and White,
    and ESRI World Imagery). Set ESRI World Imagery as default.

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -35,22 +35,28 @@ var m = L.map("map", {
         m.setView([32.69, 10.55], 3);
     }
     m.addHash();
-    var url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWx6YSIsImEiOiJjaXNzOTg0ZTcwNnBoMm9tcTVkbWd1YjJ1In0.WEDf9EZavPcZmIzgSUzVmg'
 
-    var attributionText = '&copy; <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+    // Tile Sources
+    var mq = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWx6YSIsImEiOiJjaXNzOTg0ZTcwNnBoMm9tcTVkbWd1YjJ1In0.WEDf9EZavPcZmIzgSUzVmg', {
+        attribution: '&copy; <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    var blackandwhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    var Thunderforest_Landscape = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
+        attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        apikey: '6cf6bd271b4a4e5a91e49547024e2839'
+    });
+    var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
 
-
-    var optionsObject = {
-        attribution: attributionText    }
-
-    var mq = L.tileLayer(url, optionsObject);
-    var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-    })
-    mq.addTo(m);
+    Esri_WorldImagery.addTo(m);
     var lc = L.control.layers({
-        "Stamen Watercolor": watercolor,
-        "Mapbox Streets": mq
+        "ESRI World Imagery": Esri_WorldImagery,
+        "OSM Black and White": blackandwhite,
+        "Mapbox Streets": mq,
+        "Thunderforest Landscape": Thunderforest_Landscape
     }).addTo(m);
     //make the map
     var options = {
