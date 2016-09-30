1b6b238a8a84a0118e9f5fc9a044fa049e152f57
Hayden Elza <hayden.elza@gmail.com>
2016-09-30 01:33:39 -0500

    Added Rotation
    Fixed rotation and reintegrated into the new workflow. Woot! :tada:

--- js/sampleplotter.js
+++ js/sampleplotter.js
@@ -4,28 +4,29 @@ function range(k) {
 }
 
 function genPlots(inJSON) {
+	var n = Number(document.getElementById("sample_number").value);
 	var check_topology = $('input[name="check_topology"]:checked').val();
+	var rot = Number(document.getElementById("rotation-input").value)
+	var extent = turf.extent(inJSON);
 
 	// Adjust n for topology check
-	var n = Number(document.getElementById("sample_number").value);
-	if (check_topology) {
-		var n = parseInt(n * (turf.area(turf.bboxPolygon(turf.extent(inJSON)))/turf.area(inJSON)) );
+	if (check_topology) {var n = parseInt(n * (turf.area(turf.bboxPolygon(turf.extent(inJSON)))/turf.area(inJSON)) );}
+	// Adjust n for rotation
+	if (rot != 0) {
+		var c = Math.sqrt(Math.pow(Math.abs(extent[0]-extent[2]),2)+Math.pow(Math.abs(extent[1]-extent[3]),2));
+		var n = parseInt(n*(Math.pow(c,2))/(Math.abs(extent[0]-extent[2])*Math.abs(extent[1]-extent[3])));
+		var extent = [extent[0]-((c-Math.abs(extent[0]-extent[2]))/2),extent[1]-((c-Math.abs(extent[1]-extent[3]))/2),extent[2]+((c-Math.abs(extent[0]-extent[2]))/2),extent[3]+((c-Math.abs(extent[1]-extent[3]))/2)];
 	}
-	/*if (rot != 0) {
-		var c = Math.sqrt(Math.pow(Math.abs(extent[0]-extent[1]),2)+Math.pow(Math.abs(extent[2]-extent[3]),2));
-		var n = parseInt(n*(Math.pow(c,2))/(Math.abs(extent[0]-extent[1])*Math.abs(extent[2]-extent[3])));
-		var extent = [extent[0]-((c-Math.abs(extent[0]-extent[1]))/2),extent[1]+((c-Math.abs(extent[0]-extent[1]))/2),extent[2]-((c-Math.abs(extent[2]-extent[3]))/2),extent[3]+((c-Math.abs(extent[2]-extent[3]))/2)];
-	}*/
 	
 	// Sample types
 	if ($('input[name="sample_type"]:checked').val() == "random_sample"){
-		var points = randomSample(inJSON,n);
+		var points = randomSample(inJSON,n,extent);
 	}
 	else if ($('input[name="sample_type"]:checked').val() == "systematic_sample") {
-		var points = systematicSample(inJSON,n);
+		var points = systematicSample(inJSON,n,extent);
 	}
 	else if ($('input[name="sample_type"]:checked').val() == "equidistant_sample") {
-		var points = equidistantSample(inJSON,n);
+		var points = equidistantSample(inJSON,n,extent);
 	}
 	else if ($('input[name="sample_type"]:checked').val() == undefined) {
 		alert("You must choose a sample type.")
@@ -34,9 +35,29 @@ function genPlots(inJSON) {
 	// Create output geojson
 	var outJSON = turf.featurecollection(points);
 
+	// Rotate
+	if (rot != 0) {
+		var origin  = turf.center(outJSON);
+		var originX = origin.geometry.coordinates[0];
+		var originY = origin.geometry.coordinates[1];
+		
+		var rotPoints = [];
+		for (i in range(outJSON.features.length)) {
+			var pointX = outJSON.features[i].geometry.coordinates[0];
+			var pointY = outJSON.features[i].geometry.coordinates[1];
+			var rotCoords = rotate_point(pointX, pointY, originX, originY, rot);
+			var rotPoint = turf.point(
+				[rotCoords.x, rotCoords.y],
+				{ "fid": i }
+			);
+			rotPoints.push(rotPoint)
+		}
+		var outJSON = turf.featurecollection(rotPoints);
+	}
+
 	// Check Topology
 	if (check_topology) {
-		var outJSON = turf.within(outJSON,inJSON)
+		var outJSON = turf.within(outJSON,inJSON);
 	}
 
 	// Export and Display on Map
@@ -45,8 +66,6 @@ function genPlots(inJSON) {
 
 function rotate_point(pointX, pointY, originX, originY, angle) {
 	angle = angle * Math.PI / 180.0;
-	//var originX = 15;
-	//var originY = 15;
 	return {
 		x: (Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY)) + originX,
 		y: (Math.sin(angle) * (pointX-originX) + Math.cos(angle)* (pointY-originY)) + originY
@@ -70,11 +89,9 @@ function displayOnMap(inJSON,outJSON) {
 	geojsonLayer.addTo(m);
 }
 
-function randomSample(inJSON,n) {
-	var bounds =  L.geoJson(inJSON).getBounds();
-	var extent = [bounds._southWest.lng, bounds._northEast.lng, bounds._southWest.lat, bounds._northEast.lat]  // Extent: minx, maxx, miny, maxy
-	var rangeX = extent[1]-extent[0];
-	var rangeY = extent[3]-extent[2];
+function randomSample(inJSON,n,extent) {
+	var rangeX = extent[2]-extent[0];
+	var rangeY = extent[3]-extent[1];
 
 	// Generate points
 	var points = [];
@@ -82,7 +99,7 @@ function randomSample(inJSON,n) {
 		var newPoint = turf.point(
 			[
 				extent[0]+(rangeX*Math.random()), 
-				extent[2]+(rangeY*Math.random())
+				extent[1]+(rangeY*Math.random())
 			],
 			{ "fid": fid }
 		);
@@ -91,42 +108,30 @@ function randomSample(inJSON,n) {
 	return points
 }
 
-function systematicSample(inJSON,n) {
-
-	var bounds =  L.geoJson(inJSON).getBounds();
-	var extent = [bounds._southWest.lng, bounds._northEast.lng, bounds._southWest.lat, bounds._northEast.lat]  // Extent: minx, maxx, miny, maxy
-	var rangeX = extent[1]-extent[0];
-	var rangeY = extent[3]-extent[2];
-	var centerX = (extent[0]+extent[1])/2;
-	var centerY = (extent[2]+extent[3])/2;
+function systematicSample(inJSON,n,extent) {
+	var rangeX = extent[2]-extent[0];
+	var rangeY = extent[3]-extent[1];
+	var centerX = (extent[0]+extent[2])/2;
+	var centerY = (extent[1]+extent[3])/2;
 
 	// Create object to form geojson
 	var points = [];
 
 	// Calculate variables
-	var dX = Math.max(extent[0],extent[1])-Math.min(extent[0],extent[1]);
-	var dY = Math.max(extent[2],extent[3])-Math.min(extent[2],extent[3]);
+	var dX = Math.max(extent[0],extent[2])-Math.min(extent[0],extent[2]);
+	var dY = Math.max(extent[1],extent[3])-Math.min(extent[1],extent[3]);
 	var nX = parseInt(Math.round(Math.sqrt((n*dX)/dY),0));
 	var nY = parseInt(Math.round(Math.sqrt((n*dY)/dX),0));
 	var d = dX/(nX);
 
 	var xStart = extent[0]+(d*Math.random());
-	var yStart = extent[2]+(d*Math.random());
+	var yStart = extent[1]+(d*Math.random());
 	var fid = 0;
-
-	if ($('input[name="check_topology"]:checked').val()){var check_topology = true;} else {var check_topology = false;}
 
 	for (var u in range(nX)) {
 		var x = xStart + (u*d);
 		for (var v in range(nY)) {
 			var y = yStart + (v*d);
-
-			if (rot != 0) {
-				newPoint = rotate_point(x,y,centerX,centerY,rot)
-				var x = newPoint.x;
-				var y = newPoint.y;
-				console.log(x,y)
-			}
 
 			var newPoint = turf.point(
 				[x,y],
@@ -141,27 +146,24 @@ function systematicSample(inJSON,n) {
 	return points
 }
 
-function equidistantSample(inJSON,n) {
-
-	var bounds =  L.geoJson(inJSON).getBounds();
-	var extent = [bounds._southWest.lng, bounds._northEast.lng, bounds._southWest.lat, bounds._northEast.lat]  // Extent: minx, maxx, miny, maxy
-	var rangeX = extent[1]-extent[0];
-	var rangeY = extent[3]-extent[2];
-	var centerX = (extent[0]+extent[1])/2;
-	var centerY = (extent[2]+extent[3])/2;
+function equidistantSample(inJSON,n,extent) {
+	var rangeX = extent[2]-extent[0];
+	var rangeY = extent[3]-extent[1];
+	var centerX = (extent[0]+extent[2])/2;
+	var centerY = (extent[1]+extent[3])/2;
 
 	// Create object to form geojson
 	var points = [];
 
 	// Calculate variables
-	var dX = Math.max(extent[0],extent[1])-Math.min(extent[0],extent[1]);
-	var dY = Math.max(extent[2],extent[3])-Math.min(extent[2],extent[3]);
+	var dX = Math.max(extent[0],extent[2])-Math.min(extent[0],extent[2]);
+	var dY = Math.max(extent[1],extent[3])-Math.min(extent[1],extent[3]);
 	var nX = parseInt(Math.round((-1+Math.sqrt(1-(16*(-2*Math.sqrt(2)*dX*n/dY))))/8,0));
 	var nY = parseInt(Math.round(n/nX,0));
 	var d = dX/(nX);
 
 	var xStart = extent[0]+((d/2)*Math.random());
-	var yStart = extent[2]+((d*Math.sqrt(2)/2)*Math.random());
+	var yStart = extent[1]+((d*Math.sqrt(2)/2)*Math.random());
 	var fid = 0;
 
 	for (var v in range(nY)) {
@@ -170,12 +172,6 @@ function equidistantSample(inJSON,n) {
 			if (v%2==0){
 				x = xStart + (u*d)
 
-				if (rot != 0) {
-					newPoint = rotate_point(x,y,centerX,centerY,rot)
-					var x = newPoint.x;
-					var y = newPoint.y;
-				}
-				
 				var newPoint = turf.point(
 					[x,y],
 					{ "fid": fid }
@@ -186,12 +182,6 @@ function equidistantSample(inJSON,n) {
 			}
 			else {
 				x = xStart + ((u)*d) + (d/2)
-
-				if (rot != 0) {
-					newPoint = rotate_point(x,y,centerX,centerY,rot)
-					var x = newPoint.x;
-					var y = newPoint.y;
-				}
 
 				var newPoint = turf.point(
 					[x,y],
